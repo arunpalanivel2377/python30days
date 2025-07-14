2570f5ca7f129911964b62960fc4d6836e02865c
arunpalanivel2377 <arunpalanivel2377@gmail.com>
2025-07-14 11:56:28 +0530

    Added Day

---
+++ Day2/README.md
@@ -0,0 +1,23 @@
+# 🎯 Day 02 – Number Guessing Game
+
+This is a fun **Python command-line game** where the player tries to guess a randomly generated number between 1 and 100. The game gives helpful hints after each guess and counts how many attempts the player takes to guess correctly.
+
+---
+
+## ✅ Features
+
+- 🔢 Random number generation (1–100)
+- 🔁 Continuous guessing until correct
+- 📉 Feedback: "Too low", "Too high", or "Correct"
+- 🧠 Tracks number of attempts
+- 🚫 Prevents invalid input (optional: add later)
+
+---
+
+## 🚀 How to Run
+
+1. Make sure you have Python 3 installed.
+2. Open your terminal and navigate to the project folder.
+3. Run the script with:
+   ```bash
+   python day2.py
---
+++ Day2/day2.py
@@ -0,0 +1,48 @@
+"""
+# 🎯 Day 02 – Number Guessing Game
+
+This is a fun **Python command-line game** where the player tries to guess a randomly generated number between 1 and 100. The game gives helpful hints after each guess and counts how many attempts the player takes to guess correctly.
+
+---
+
+## ✅ Features
+
+- 🔢 Random number generation (1–100)
+- 🔁 Continuous guessing until correct
+- 📉 Feedback: "Too low", "Too high", or "Correct"
+- 🧠 Tracks number of attempts
+- 🚫 Prevents invalid input (optional: add later)
+
+"""
+
+import random
+from wsgiref.util import guess_scheme
+
+
+def random_number():
+    print("=== Random Number Guessing Game ===")
+    random_number = random.randint(1, 100)
+    attempts = 0
+    guess = None
+
+    while guess != random_number:
+        try:
+            guess = int(input("Guess: "))
+            attempts += 1
+            if guess < 1 or guess > 100:
+                print("Please enter a number between 1 and 100")
+                continue
+
+            if guess < random_number:
+                print("Too low")
+            elif guess > random_number:
+                print("Too high")
+            elif guess == random_number:
+                print("Correct")
+        except:
+            print("Provide valid input")
+    print("You took " + str(attempts) + " attempts")
+    print("Thank you for playing")
+
+
+random_number()
